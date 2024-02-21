61b85dd872945c6fa59c5510df34cf38d9f48b39
elcamisas <setadelfortnite@gmail.com>
2024-02-21 16:44:28 +0100

    sasa

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function moveSlide(n) {
+    const slides = document.querySelectorAll('.carousel img');
+    slideIndex += n;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+
+function cambiarPagina(select) {
+    var selectedOption = select.options[select.selectedIndex].value;
+    if (selectedOption) {
+        window.location.href = selectedOption;
+    }
+}
